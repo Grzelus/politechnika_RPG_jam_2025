e59b42fcf32b5e7ffa48c7d7e574ae9fd4a5ca1f
Kacper <grzelakdev@gmail.com>
2025-11-14 22:20:29 +0100

    title layout

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,9 +1,18 @@
 extends Node2D
 
 
+@onready var button: Button = get_node_or_null("ColorRect/Button")
+
+
+func on_click():
+	print("clicked")
+	get_tree().change_scene_to_file("res://scenes/Level_2.tscn")
+	
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	button.connect("pressed", Callable(self, "on_click"))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
