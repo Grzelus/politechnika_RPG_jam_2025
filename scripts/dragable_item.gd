da1498fb4ea5aa920f67bce6741d80f9c863a907
Kacper <grzelakdev@gmail.com>
2025-11-16 12:17:20 +0100

    toggle_sprite added

--- scripts/dragable_item.gd
+++ scripts/dragable_item.gd
@@ -4,14 +4,22 @@ extends RigidBody2D
 @export var card_id: String
 
 @onready var sprite := get_node_or_null("Sprite2D")
+var reverse = preload("res://assets/card_bg.png")
 @onready var collision_shape = get_node_or_null("CollisionShape2D").shape
 
 var dragging = false
 var of = Vector2(0.0, 0.0)
 
+func toggle_sprite():
+	if sprite.texture == texture:
+		sprite.texture = reverse
+	else:
+		sprite.texture = texture
+
 func _process(_delta: float) -> void:
 	if dragging:
 		position = get_global_mouse_position() - of
+		
 
 func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 		if event.is_action_pressed("left_click"):
@@ -20,6 +28,8 @@ func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int
 			of = get_global_mouse_position() - global_position
 		if event.is_action_released("left_click"):
 			dragging = false
+		if event.is_action_pressed("right_click"):
+			toggle_sprite()
 func _ready() -> void:
 	if texture:
 		var texture_size = texture.get_size()
